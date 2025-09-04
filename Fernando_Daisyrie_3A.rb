31c93074233e678271e909cb4fcca982ebcde9df
HEZRON GAGARIN BSCS2A <hezrongg26@gmail.com>
2025-09-04 20:09:29 +0800

    Fernando_Daisyrie_3A.rb

---
+++ Fernando_Daisyrie_3A.rb
@@ -0,0 +1,30 @@
+#Activity1
+
+full_name = "Daisyrie Fernando"
+age = 20
+course = "Bachelor of Science in Computer Science"
+address = "Talogtog, Candon City"
+grade = 90.5
+currently_enrolled = true
+
+puts "full_name: #{full_name}"
+puts "age: #{age}"
+puts "course: #{course}"
+puts "address: #{address}"
+puts "grade: #{grade}"
+puts "currently_enrolled: #{currently_enrolled}"
+
+#Activity2
+#Array
+
+Bangtan_members = ["Taehyung", "Jimin", "Jungkook"]
+Bangtan_age = ["28", "29", "27"]
+
+puts "Bangtan_members: #{Bangtan_members[0]}"
+
+puts "Bangtan_age: #{Bangtan_age[0]}"
+
+#hash
+student_info = {name: "Patricia", age: 19, weight: 41.5, course: "Bachelor of Science in Computer Science", address: "Balingaoan,Candon City" , has_scholarship: true}
+
+puts "type: #{student_info.class}"
