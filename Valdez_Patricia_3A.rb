19fe9296e505ba1c5f3bbcb8278f5c4e2485d3e8
HEZRON GAGARIN BSCS2A <hezrongg26@gmail.com>
2025-09-04 19:41:03 +0800

    Valdez_Patricia_3A.rb

---
+++ Valdez_Patricia_3A.rb
@@ -0,0 +1,34 @@
+# Activity 1
+
+full_name = "Patricia Valdez"
+age = 19
+course = "BSCS"
+address = "Balingaoan Candon City Ilocos Sur"
+grade = 90.2
+currently_enrolled = true
+
+puts "full_name: #{full_name}"
+puts "age: #{age}"
+puts "course: #{course}"
+puts "address: #{address}"
+puts "grade: #{grade}"
+puts "currently_enrolled: #{currently_enrolled}"
+
+
+
+
+# Activity 2
+#Array
+enhypen_member = ["Jay", "Sunghoon", "Sunoo", "Ni-ki"]
+enhypen_age = [20, 21, 23, 19]
+
+puts "enhypen_member: #{enhypen_member[0]}"
+
+puts "enhypen_age: #{enhypen_age[0]}"
+
+#Hash
+
+student_info = {name: "Daisyrie", age: 20, course: "BSCS", address: "Talogtog Candon City Ilocos Sur", weight: 46.5, has_scholarship: false}
+
+
+puts "type: #{student_info.class}"
