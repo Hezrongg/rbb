35a614b4cc9854abe75a2859cf97be8622450603
HEZRON GAGARIN BSCS2A <hezrongg26@gmail.com>
2025-09-05 16:52:29 +0800

    Cortez_Angelette_3A.rb

---
+++ Cortez_Angelette_3A.rb
@@ -0,0 +1,30 @@
+#Activity1
+
+full_name = "Keycee Hallascon"
+age = 25
+course = "Bachelor of Science in Computer Science"
+address = "Sta. Lucia"
+grade = 95.5
+currently_enrolled = true
+
+puts "full_name: #{full_name}"
+puts "age: #{age}"
+puts "course: #{course}"
+puts "address: #{address}"
+puts "grade: #{grade}"
+puts "currently_enrolled: #{currently_enrolled}"
+
+#Activity2
+#Array
+
+Sangre_members = ["Tera", "Suma", "Pirena"]
+Sangre_age = ["21", "60", "35"]
+
+puts "Sangre_members: #{Sangre_members[0]}"
+
+puts "Sangre_age: #{Sangre_age[0]}"
+
+#hash
+student_info = {name: "Angelette", age: 20, weight: 40, course: "Bachelor of Science in Computer Science", address: "Bagani Campo, Candon City" , has_scholarship: true}
+
+puts "type: #{student_info.class}"
